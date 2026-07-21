1ab8e1b220b9fe5d9d440580f3a1461098718340
taharzerrioui-web <tahar.zerrioui@creditplus.ma>
2026-07-21 15:47:03 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Invitation-mariage-tahar-majda
-Invitation mariage Tahar &amp; Majda
+# Arin & Miftah Wedding Website
+
+![Arin Miftah Wedding](https://github.com/Miftahussalam/wedding/blob/master/src/assets/thumbnail.png?raw=true)
+
+Features:
+- Landing Page, with features like:
+ - Countdown Timer
+ - Floating Music
+ - Photo Gallery
+ - Send Message
+ - And more..
+- also, this web-app has QR Guest Generator
+- and Guest Tikcet Viewer
+
+Tech Stack:
+- GatsbyJs
+
+Credits
+- Original Landing Page Template (https://freehtml5.co/)
+- Icon Assets (https://www.flaticon.com/packs/covid-protection-measures-5)
+- Indra Kusuma https://github.com/idindrakusuma
+
+Thanks!
---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  pathPrefix: `/wedding`,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-eslint',
+    `gatsby-plugin-emotion`,
+    'gatsby-optional-chaining',
+    'clipboard-copy',
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        aliases: {
+          '@components': 'src/components',
+          '@helpers': 'src/helpers',
+          '@pages': 'src/pages',
+          '@assets': 'src/assets',
+          '@hooks': 'src/hooks',
+          '@': 'src',
+        },
+      },
+    },
+  ],
+};
